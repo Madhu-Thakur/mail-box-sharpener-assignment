91953d8d1f224523d8b145b7432c1dfe3dd519bd
Madhu <madhuthakur2321998@gmail.com>
2026-06-06 16:46:11 +0530

    Added login authentication and welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
+import LoginForm from "./components/LoginForm";
 import SignupForm from "./components/SignupForm";
+import Welcome from "./components/Welcome";
+import "./App.css";
 
 function App() {
-  return <SignupForm />;
+  const isLoggedIn =
+    localStorage.getItem("isLoggedIn");
+
+  const [showLogin, setShowLogin] = useState(true);
+
+  if (isLoggedIn) {
+    return <Welcome />;
+  }
+
+  return showLogin ? (
+    <LoginForm onToggle={() => setShowLogin(false)} />
+  ) : (
+    <SignupForm onToggle={() => setShowLogin(true)} />
+  );
 }
 
 export default App;
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,13 @@
+function Welcome() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h3>
+        Welcome to Expense Tracker!!!
+      </h3>
+      <hr />
+    </div>
+  );
+}
+
+export default Welcome;
+
